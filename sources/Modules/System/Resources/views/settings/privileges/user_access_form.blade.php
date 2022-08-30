ad9d0218afd7a3f9e62012baaf83fc0834e4a151
yoda062100097 <Yoda062100097@byl01-pbtgroup.com>
2022-08-30 16:55:55 +0700

    30-08-2022

--- sources/Modules/System/Resources/views/settings/privileges/user_access_form.blade.php
+++ sources/Modules/System/Resources/views/settings/privileges/user_access_form.blade.php
@@ -2,108 +2,211 @@
 @section('title', $title)
 
 @section('content')
-<div class="row">
-    <div class="col-md-10 offset-1">
-        <div class="card card-default">
-            <div class="card-header">
-                <h3 class="card-title">{{ $title }}</h3>
-            </div>
-            <!-- /.card-header -->
-            <form action="{{ $action }}" method="post" id="privilege_form">
-                {{ csrf_field() }}
-                <input name="privilege_id" value="{{Crypt::encrypt($privilege_id)}}" type="hidden">
+    <div class="row">
+        <div class="col-md-10 offset-1">
+            <div class="card card-default">
+                <div class="card-header">
+                    <h3 class="card-title">{{ $title }}</h3>
+                </div>
+                <!-- /.card-header -->
+                <form action="{{ $action }}" method="post" id="privilege_form">
+                    {{ csrf_field() }}
+                    <input name="privilege_id" value="{{ Crypt::encrypt($privilege_id) }}" type="hidden">
 
-                <div class="card-body">
-                    <div class="form-group">
-                        <label>NIK Karyawan</label>
-                        <input value="{{$privilege_user_nik}}" name="privilege_user_nik" type="text" class="form-control" placeholder="ex : Avanza, Terios.." readonly>
-                    </div>
+                    <div class="card-body">
+                        <div class="form-group">
+                            <label>Pilih Jenis</label>
+                            <select name="jenis" id="jenis" class="form-control">
+                                <option value="internal" {{ $jenisku == 'internal' ? 'selected' : '' }}>Internal</option>
+                                <option value="external" {{ $jenisku == 'external' ? 'selected' : '' }}>External</option>
+                            </select>
+                        </div>
 
-                    <div class="form-group">
-                        <label>Nama Karyawan</label>
-                        <input value="{{$privilege_user_name}}" name="privilege_user_name" type="text" class="form-control" placeholder="ex : Avanza, Terios.." readonly>
-                    </div>
-                    <div class="form-group">
-                        <label>Grup Akses</label>
-                        <select name="privilege_group_access_id" class="form-control select2" style="width: 100%;">
-                            <option value="">-- Pilih Akses --</option>
-                            @foreach ($group_access_data as $ga)
-                            @php
-                            if (isset($privilege_group_access_id) && $privilege_group_access_id == $ga->group_access_id) {
-                            $selected = 'selected';
-                            }else{
-                            $selected = '';
-                            }
-                            @endphp
-                            <option value="{{ $ga->group_access_id }}" {{$selected}}>{{ $ga->group_access_name }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="form-group">
-                        <label>Location</label>
-                        <select name="privilege_user_location[]" class="select2" multiple="multiple" data-placeholder="Pilih lokasi" style="width: 100%;">
-                            @foreach ($factory_data as $data)
-                            @if ($privilege_user_location != '')
-                            @php ($select = in_array($data->factory_name, explode(',', $privilege_user_location)))
-                            @php ( $select ? $selected = 'selected' : $selected = '')
-                            @else
-                            @php ($selected = '')
-                            @endif
-                            <option value="{{$data->factory_name}}" {{$selected}}>
-                                {{$data->factory_name}}
-                            </option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="row align-items-center h-100">
-                        <div class="col-6">
+
+                        <div id="internalku">
                             <div class="form-group">
-                                <label>API Key</label>
-                                <input id="privilege-api-key" value="{{$privilege_api_key}}" name="privilege_api_key" type="text" class="form-control" readonly>
+                                <label>NIK Karyawan</label>
+                                <input value="{{ $privilege_user_nik }}" name="internalnik" type="text"
+                                    class="form-control" id="internalnik" placeholder="masukkan nik">
+                            </div>
+
+                            <div class="form-group">
+                                <label>Nama Karyawan</label>
+                                <input value="{{ $privilege_user_name }}" name="internalnama" type="text"
+                                    class="form-control" id="internalnama" placeholder="nama" readonly>
                             </div>
                         </div>
-                        <div class="col-6">
-                            <button id="btn-api" type="button" class="btn btn-primary"><i class="fas fa-key"></i> Generate Key</button>
+
+                        <div id="externalku">
+                            <div class="form-group">
+                                <label>Email</label>
+                                <input value="{{ $privilege_user_nik }}" name="externalemail" type="text"
+                                    class="form-control" id="externalemail" placeholder="masukkan email">
+                            </div>
+
+                            <div class="form-group">
+                                <label>Nama</label>
+                                <input value="{{ $privilege_user_name }}" name="externalnama" type="text"
+                                    class="form-control" id="externalnama" placeholder="masukkan nama">
+                            </div>
+
+                            <div class="form-group">
+                                <label>NIK Validasi Finance</label>
+                                <input value="{{ $nik_finance }}" name="nikfinance" type="text" class="form-control"
+                                    id="nikfinance" placeholder="NIK Finance">
+                            </div>
+                            <div class="form-group">
+                                <label>Nama Finance</label>
+                                <input value="{{ $nama_finance }}" name="namafinance" type="text" class="form-control"
+                                    id="namafinance" placeholder="nama Finance" readonly>
+                            </div>
+                            <div class="form-group">
+                                <label>Email Finance</label>
+                                <input value="{{ $email_finance }}" name="emailfinance" type="email" class="form-control"
+                                    id="emailfinance" placeholder="Email Finance">
+                            </div>
                         </div>
+
+
+                        <div class="form-group">
+                            <label>Grup Akses</label>
+                            <select name="privilege_group_access_id" class="form-control select2" style="width: 100%;">
+                                <option value="" disabled>-- Pilih Akses --</option>
+                                @foreach ($group_access_data as $ga)
+                                    @php
+                                        if (isset($privilege_group_access_id) && $privilege_group_access_id == $ga->group_access_id) {
+                                            $selected = 'selected';
+                                        } else {
+                                            $selected = '';
+                                        }
+                                    @endphp
+                                    <option value="{{ $ga->group_access_id }}" {{ $selected }}>
+                                        {{ $ga->group_access_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+
                     </div>
-                </div>
-                <div class="card-footer">
-                    <a href="{{ url('privilege/user_access') }}" class="btn btn-default">Kembali</a>
-                    <button type="submit" class="btn btn-info float-right">Simpan</button>
-                </div>
-            </form>
+                    <div class="card-footer">
+                        <a href="{{ url('privilege/user_access') }}" class="btn btn-default">Kembali</a>
+                        <button type="submit" class="btn btn-info float-right">Simpan</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 @endsection
 
 @section('script')
-<script>
-    $(document).ready(function(){
+    <script>
+        $(document).ready(function() {
+            var jenisku = @JSON($jenisku);
+            console.log('jenisku :>> ', jenisku);
+            if (jenisku == 'internal') {
+                $("#internalku").show();
+                $("#externalku").hide();
+            } else {
+                $("#externalku").show();
+                $("#internalku").hide();
+            }
 
-        load_event()
+            // $("#internalku").show();
+            // $("#externalku").hide();
+            $("#internalnik").attr('required', true);
+            $("#internalnama").attr('required', true);
+            $("#externalnik").attr('required', false);
+            $("#externalnama").attr('required', false);
+            $("#nikfinance").attr('required', false);
+            $("#namafinance").attr('required', false)
+            $("#emailfinance").attr('required', false)
+            $("#jenis").change(function() {
+                var jenis = $("#jenis").val();
+                if (jenis == "internal") {
+                    $("#internalku").show();
+                    $("#externalku").hide();
+                    $("#internalnik").attr('required', true);
+                    $("#internalnama").attr('required', true);
+                    $("#externalnik").attr('required', false);
+                    $("#externalnama").attr('required', false);
+                    $("#nikfinance").attr('required', false);
+                    $("#namafinance").attr('required', false)
+                    $("#emailfinance").attr('required', false)
+                } else if (jenis == 'external') {
+                    $("#internalku").hide();
+                    $("#externalku").show();
+                    $("#internalnik").attr('required', false);
+                    $("#internalnama").attr('required', false);
+                    $("#externalnik").attr('required', true);
+                    $("#externalnama").attr('required', true);
+                    $("#nikfinance").attr('required', true);
+                    $("#namafinance").attr('required', true);
+                    $("#emailfinance").attr('required', true)
+                } else {
+                    $("#internalku").hide();
+                    $("#externalku").hide();
+                    $("#internalnik").attr('required', false);
+                    $("#internalnama").attr('required', false);
+                    $("#externalnik").attr('required', false);
+                    $("#externalnama").attr('required', false);
+                    $("#nikfinance").attr('required', false);
+                    $("#namafinance").attr('required', false)
+                    $("#emailfinance").attr('required', false)
+                }
+            });
 
-        function load_event(){
-            btnapi_event()
-        }
+            $("#internalnik").change(function() {
+                var nik = $("#internalnik").val();
+                var token = $('meta[name=csrf-token]').attr('content');
+                console.log('nik :' + nik);
+                $.ajax({
+                    url: "<?php echo route('privileggetnama'); ?>",
+                    method: 'POST',
+                    data: {
+                        nik: nik,
+                        _token: token
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        if (data == '-') {
+                            alert('Nik Not Found');
+                            $("#internalnik").val('');
+                            $("#internalnama").val('');
+                        } else {
+                            $("#internalnama").val(data);
+                        }
+                        cache: false
+                    }
+                });
 
-        function btnapi_event(){
-            $('#btn-api').click(function(e){
-                let key = generate_token()
+            });
 
-                $('#privilege-api-key').val(key)
-            })
-        }
 
-        function generate_token(){
-            let unixtimestamps = Date.now().toString().substr(-4)
+            $("#nikfinance").change(function() {
+                var nik = $("#nikfinance").val();
+                var token = $('meta[name=csrf-token]').attr('content');
+                console.log('nik :' + nik);
+                $.ajax({
+                    url: "<?php echo route('privileggetnama'); ?>",
+                    method: 'POST',
+                    data: {
+                        nik: nik,
+                        _token: token
+                    },
+                    success: function(data) {
+                        console.log(data);
+                        if (data == '-') {
+                            alert('Nik Not Found');
+                            $("#nikfinance").val('');
+                            $("#namafinance").val('');
+                        } else {
+                            $("#namafinance").val(data);
+                        }
+                        cache: false
+                    }
+                });
 
-            let token = Math.random().toString(36).substr(2)
+            });
 
-            let key = unixtimestamps + token
-
-            return key
-        }
-    })
-</script>
+        })
+    </script>
 @endsection
