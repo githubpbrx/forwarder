1cfca4aac15e0c546b749f99017057d5bf75311d
yoda062100097 <Yoda062100097@byl01-pbtgroup.com>
2022-09-20 15:12:44 +0700

    ganti bhs inggris aktifasiemail

--- sources/Modules/System/Resources/views/login/emailaktifasi.blade.php
+++ sources/Modules/System/Resources/views/login/emailaktifasi.blade.php
@@ -1,27 +1,26 @@
 @extends('system::template/masteremail')
 @section('link_href')
- 
 @endsection
 
 @section('content')
+    <br>
+    <p>Hi <b><i>,</i> {{ $nama }}</b></p>
 
-<br>
-<p>Hi <b><i>,</i> {{$nama}}</b></p>
-
-<p>Berikut adalah LINK untuk Aktifasi User <b>Web Forwarder</b> atau masukkan kode Token dibawah:
-<center>
-<a href="{{$link}}"><button style="background-color:  #6495ED; color: white; font-weight: bold; width:140px; height: 34px; border-radius: 11px"> LINK AKTIFASI </button></a>
-<br><br> atau <br><b style="font-size:30pt">{{$token}}</b>
-</center>
-<br>
-<p>Silahkan melakuakan aktifasi user sebelum menggunakan Web forwarder.
-</p>
-<b>Jika anda tidak request dari Web FORWARDER silahkan abaikan email ini</b>
+    <p>Here is the LINK for User Activation <b>Web Forwarder</b> or enter the token code below:
+        <center>
+            <a href="{{ $link }}"><button
+                    style="background-color:  #6495ED; color: white; font-weight: bold; width:140px; height: 34px; border-radius: 11px">
+                    LINK ACTIVATION </button></a>
+            <br><br> or <br><b style="font-size:30pt">{{ $token }}</b>
+        </center>
+        <br>
+    <p>Please activate the user before using the Web forwarder.
+    </p>
+    <b>If you don't request from Web FORWARDER, please ignore this email</b>
 @endsection
 
 @section('script_src')
 @endsection
 
 @section('script')
-
 @endsection
