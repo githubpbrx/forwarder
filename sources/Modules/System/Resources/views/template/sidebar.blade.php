ba13c1fc93e2c2bb942c963c46804392ed841e35
yoda062100097 <Yoda062100097@byl01-pbtgroup.com>
2022-09-23 11:14:30 +0700

    add menu reportforwarder in sidebar

--- sources/Modules/System/Resources/views/template/sidebar.blade.php
+++ sources/Modules/System/Resources/views/template/sidebar.blade.php
@@ -139,6 +139,15 @@
                     </li>
                 @endif
 
+                {{-- @if (RoleAccess::whereMenu(11) > 0)
+                    <li class="nav-item">
+                        <a href="{{ route('report_forwarder') }}" class="nav-link">
+                            <i class="fa fa-file nav-icon"></i>
+                            <p>Report Forwarder</p>
+                        </a>
+                    </li>
+                @endif --}}
+
                 @if ($menu_session != '')
                     @include($menu_session . '::template/' . $menu_session . '_sidebar')
                 @endif
