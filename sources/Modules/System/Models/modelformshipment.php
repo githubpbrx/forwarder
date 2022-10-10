ab6d9e075df9fbef073717f66abb0425f4141836
yoda062100097 <Yoda062100097@byl01-pbtgroup.com>
2022-10-10 08:39:01 +0700

    add model formshipment

---
+++ sources/Modules/System/Models/modelformshipment.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Modules\System\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class modelformshipment extends Model
+{
+    protected $table = 'formshipment';
+    protected $primaryKey = 'id_shipment';
+    protected $guarded = [];
+}
